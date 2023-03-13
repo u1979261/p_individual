a8f22fdb71d22bcd910a751d31f3465512e30b6e
Ramon Martinez Rodriguez <u1979261@campus.udg.edu>
2023-03-13 17:41:05 +0100

    P3

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function loadpage(url){
     window.location.assign(url);
 }
-function go_game(){
+function go_game2(){
     loadpage ("./pi_2/index.html")
 }
 function Menu(){
     loadpage ("../index.html")
+}
+function go_game3(){
+    loadpage ("./pi_3/index.html")
 }
